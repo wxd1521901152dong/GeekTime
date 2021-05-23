f72c9aa66d26cd422d0029caf19d3a62c80195c3
wxd1521901152dong <18510085722@163.com>
2021-05-23 22:15:17 +0800

    提交第三周作业

---
+++ week3/HttpHandler.java
@@ -0,0 +1,118 @@
+package com.geek.world.week2.netty;
+
+import com.geek.world.week2.socket.HttpClientTest1;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * <h3>geek</h3>
+ *
+ * @classname:HttpHandler
+ * @author:xiaodong
+ * @date:2021-05-12 21:08:06
+ * @description:
+ */
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+
+
+
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx );
+            } else {
+                handlerTest(fullRequest, ctx );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        FullHttpResponse response = null;
+        try {
+            // 对接上次作业的httpclient或者okhttp请求另一个url的响应数据
+            String value = querybusinessInfo();
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+
+        } catch (Exception e) {
+            System.out.println("处理错误:" + e.getMessage());
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+
+    }
+
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    private String querybusinessInfo() throws IOException {
+        String url = "http://localhost:8801";
+        String text = getAsString(url);
+        System.out.println("url: " + url + " ; response: \n" + text);
+        return text;
+    }
+
+    // GET 调用
+    public  String getAsString(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response1 = null;
+        try {
+            response1 = httpclient.execute(httpGet);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            return EntityUtils.toString(entity1, "UTF-8");
+        } finally {
+            if (null != response1) {
+                response1.close();
+            }
+        }
+    }
+}
